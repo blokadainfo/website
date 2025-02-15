40d83c6713bbbc1ee3e8d22ae0a3f2ab6cfeb2c7
Matija Kljajić <64456143+matijakljajic@users.noreply.github.com>
2025-02-15 03:04:33 +0100

    feat: background filter

--- src/lib/components/custom/hero/hero.svelte
+++ src/lib/components/custom/hero/hero.svelte
@@ -32,20 +32,18 @@
 
 <div class="hero relative min-h-screen">
 	<div
-		class="absolute inset-0 bg-cover bg-center transition-opacity duration-1000"
+		class="absolute inset-0 bg-cover bg-center transition-opacity duration-1000 grayscale brightness-50"
 		style="background-image: url('{imageUrlA}'); opacity: {isImageVisibleA ? 1 : 0};"
 	></div>
 	<div
-		class="absolute inset-0 bg-cover bg-center transition-opacity duration-1000"
+		class="absolute inset-0 bg-cover bg-center transition-opacity duration-1000 grayscale brightness-50"
 		style="background-image: url('{imageUrlB}'); opacity: {!isImageVisibleA ? 1 : 0};"
 	></div>
 	<div class="hero-overlay bg-opacity-60"></div>
 	<div class="hero-content text-center text-neutral-content">
-		<div class="sm:max-w-lg">
-			<h1 class="mb-5 text-7xl font-bold">БОРБА НИЈЕ ГОТОВА ДОК СЕ ЗАХТЕВИ НЕ ИСПУНЕ</h1>
-			<div class="flex w-full justify-center">
-				<Timer targetDate={'2025-02-15T09:00:00Z'} />
-			</div>
+		<div class="max-w-lg sm:max-w-2xl">
+			<h1 class="mb-5 text-5xl font-bold">БОРБА НИЈЕ ГОТОВА ДОК СЕ ЗАХТЕВИ НЕ ИСПУНЕ</h1>
+			<div class="flex w-full justify-center"><Timer targetDate={'2025-02-15T09:00:00Z'} /></div>
 		</div>
 	</div>
 </div>
