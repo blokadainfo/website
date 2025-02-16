67695624110d4c2af73844e8e07795c9fa80e4cf
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2025-02-16 22:10:20 +0100

    fix(home): remove kragujevac

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
 	import Hero from '$lib/components/custom/hero/hero.svelte';
-	import Kragujevac from '$lib/components/custom/kragujevac/kragujevac.svelte';
 </script>
 
 <Hero />
-<Kragujevac />
