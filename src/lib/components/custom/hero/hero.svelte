535c859147d46640d0d2ef49cca4ceeb19e6ce07
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2025-02-17 21:57:25 +0100

    fix(hero): 15h UTC+1

--- src/lib/components/custom/hero/hero.svelte
+++ src/lib/components/custom/hero/hero.svelte
@@ -13,7 +13,7 @@
 				BORBA NIJE GOTOVA DOK SE ZAHTEVI NE ISPUNE
 			</h1>
 			<div class="flex w-full justify-center">
-				<Timer targetDate={'2025-03-01T00:00:00Z'} />
+				<Timer targetDate={'2025-03-01T14:00:00Z'} />
 			</div>
 		</div>
 	</div>
