27de54d0e12d562a9d2d87ac05aff90b003146b6
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2025-02-17 08:26:14 +0100

    fix(hero): backgrounds proper

---
+++ src/lib/components/custom/hero/backgrounds.svelte
@@ -0,0 +1,77 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		const interval = setInterval(changeBackground, 3000);
+		return () => clearInterval(interval);
+	});
+
+	const imageModules: Record<string, any> = import.meta.glob(
+		'$lib/assets/backgrounds/**/*.{avif,gif,heif,jpeg,jpg,png,tiff,webp,svg}',
+		{
+			eager: true, // TODO?
+			query: {
+				enhanced: true
+			}
+		}
+	);
+
+	const imagesNum = Object.keys(imageModules).length;
+	if (imagesNum < 1) {
+		throw new Error('at least one image must exist');
+	}
+
+	let imageIndexA = $state(0);
+	let imageIndexB = $state(imagesNum === 1 ? 0 : 1);
+	let isImageAVisible = $state(true);
+
+	const imageA = $derived(
+		Object.keys(imageModules).find((_value, index, _obj) => index === imageIndexA)
+	);
+	const imageB = $derived(
+		Object.keys(imageModules).find((_value, index, _obj) => index === imageIndexB)
+	);
+
+	function getRandomImageIndex(): number {
+		if (imagesNum < 3) {
+			throw new Error('cannot get random image index when imagesNum is less than 3');
+		}
+
+		while (true) {
+			const randomIndex = Math.floor(Math.random() * imagesNum) + 1;
+			if (randomIndex !== imageIndexA && randomIndex !== imageIndexB) {
+				return randomIndex;
+			}
+		}
+	}
+
+	function changeBackground(): void {
+		if (imagesNum === 1) return;
+		if (imagesNum === 2) {
+			isImageAVisible = !isImageAVisible;
+			return;
+		}
+
+		if (isImageAVisible) {
+			imageIndexB = getRandomImageIndex();
+		} else {
+			imageIndexA = getRandomImageIndex();
+		}
+		isImageAVisible = !isImageAVisible;
+	}
+</script>
+
+<img
+	src={imageA}
+	alt="Background A"
+	class="size-full object-cover brightness-50 grayscale transition-opacity duration-1000"
+	class:opacity-0={!isImageAVisible}
+	loading="eager"
+/>
+<img
+	src={imageB}
+	alt="Background B"
+	class="size-full object-cover brightness-50 grayscale transition-opacity duration-1000"
+	class:opacity-0={isImageAVisible}
+	loading="eager"
+/>
--- src/lib/components/custom/hero/hero.svelte
+++ src/lib/components/custom/hero/hero.svelte
@@ -1,44 +1,10 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
+	import Backgrounds from './backgrounds.svelte';
 	import Timer from '$lib/components/custom/timer/timer.svelte';
-
-	const imagesNum = 9;
-	function getRandomImageIndex(): number {
-		return Math.floor(Math.random() * imagesNum) + 1;
-	}
-
-	let imageIndexA = $state(getRandomImageIndex());
-	let imageIndexB = $state(getRandomImageIndex());
-	let isImageVisibleA = $state(true);
-
-	const imageUrlA = $derived(`/backgrounds/${imageIndexA}.jpg`);
-	const imageUrlB = $derived(`/backgrounds/${imageIndexB}.jpg`);
-
-	function changeBackground(): void {
-		if (isImageVisibleA) {
-			imageIndexB = getRandomImageIndex();
-		} else {
-			imageIndexA = getRandomImageIndex();
-		}
-
-		isImageVisibleA = !isImageVisibleA;
-	}
-
-	onMount(() => {
-		const interval = setInterval(changeBackground, 3000);
-		return () => clearInterval(interval);
-	});
 </script>
 
-<div class="hero relative h-[90lvh]">
-	<div
-		class="absolute inset-0 bg-cover bg-center brightness-50 grayscale transition-opacity duration-1000"
-		style="background-image: url('{imageUrlA}'); opacity: {isImageVisibleA ? 1 : 0};"
-	></div>
-	<div
-		class="absolute inset-0 bg-cover bg-center brightness-50 grayscale transition-opacity duration-1000"
-		style="background-image: url('{imageUrlB}'); opacity: {!isImageVisibleA ? 1 : 0};"
-	></div>
+<div class="hero h-[90lvh]">
+	<Backgrounds />
 	<div class="hero-overlay bg-opacity-60"></div>
 	<div class="hero-content text-center text-neutral-content">
 		<div class="max-w-lg sm:max-w-2xl">
