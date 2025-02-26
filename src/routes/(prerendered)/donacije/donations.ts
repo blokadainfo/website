1c119fa397a976ccd24518614b71e9daee5a1222
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2025-02-26 04:10:28 +0100

    feat: donacije

---
+++ src/routes/(prerendered)/donacije/donations.ts
@@ -0,0 +1,81 @@
+export const donations: {
+	name: string;
+	pieces?: number;
+	links?: string[];
+}[] = [
+	{
+		name: 'Gift kartice za gorivo'
+	},
+	{
+		name: 'Filter za kameru - KF Concept 82mm XV56 Nano-X Slim Variable NDX HD ND2-ND400',
+		pieces: 1,
+		links: [
+			'https://josipovic.rs/proizvod/kf-concept-82mm-xv56-nano-x-slim-variable-fader-ndx-hd-nd2-nd400-waterproof-anti-scratch-green-coated'
+		]
+	},
+	{
+		name: 'Elgato Cam Link',
+		pieces: 1,
+		links: ['https://ipon.hu/shop/termek/elgato-cam-link-4k/1712387']
+	},
+	{
+		name: 'Čitač za kartice - TRANSCEND TS-RDF8K2',
+		pieces: 1,
+		links: ['https://gigatron.rs/citaci-kartica/transcend-tsrdf8k2-218261']
+	},
+	{
+		name: 'BASEUS Metal Gleam Series 7 hub v2 Gigabit RJ45',
+		pieces: 2,
+		links: ['https://ipon.hu/shop/termek/baseus-metal-gleam-series-7-az-1-ben-hub-v2/2065078']
+	},
+	{
+		name: 'USB C RJ45 adapter Gigabitan 1000Mbps',
+		pieces: 3,
+		links: [
+			'https://gigatron.rs/mrezne-karte-i-adapteri/linkom-usbc-na-rj-45-linkom786-adapter-537215'
+		]
+	},
+	{
+		name: 'UBIQUITY airMAX LiteBeam 5AC',
+		pieces: 6,
+		links: [
+			'https://gigatron.rs/access-point/ubiquity-airmax-litebeam-5ac-spoljasnja-cpe-antena-322944'
+		]
+	},
+	{
+		name: 'TP-Link Archer AX23',
+		pieces: 5,
+		links: [
+			'https://ipon.hu/shop/termek/tp-link-archer-ax23-ax1800-dual-band-wi-fi-6-router/1941464',
+			'https://gigatron.rs/ruteri/tplink-ruter-archer-ax23-436770'
+		]
+	},
+	{
+		name: 'Eksterni SSD 2TB USB 3.1',
+		pieces: 1,
+		links: [
+			'https://www.bcgroup-online.com/eksterni-ssd/kingston-portable-xs1000-2tb-eksterni-ssd-sxs1000%5c2000g-179263',
+			'https://gigatron.rs/eksterni-hdd-i-ssd/samsung-portable-t5-evo-2tb-black-muph2t0s-eksterni-ssd-615451'
+		]
+	},
+	{
+		name: 'UPS 600-1200W',
+		pieces: 2,
+		links: [
+			'https://gigatron.rs/ups-uredjaji/njoy-hours-plus-2000-1200w-ups-pwup-li200h1-az01b-174994'
+		]
+	},
+	{
+		name: 'UGREEN PB720 Powerbank 20000mAh 100W',
+		pieces: 2,
+		links: ['https://ipon.hu/shop/termek/ugreen-pb720-powerbank-20000mah-100w-fekete/2301480']
+	},
+	{
+		name: 'Powerbank 20000-30000mAh 65W',
+		pieces: 1,
+		links: [
+			'https://ipon.hu/shop/termek/sandberg-survivor-powerbank-27000-pd65w/2318326',
+			'https://gigatron.rs/power-bank-eksterne-baterije/usams-30000mah-65w-power-bank-+-usb-typec-kabl-684579'
+		]
+	}
+];
