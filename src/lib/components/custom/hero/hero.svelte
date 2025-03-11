24a94d465467b39e70dc299653fe0ea8162c538d
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2025-03-11 05:59:42 +0100

    fix(hero): rtv 2
    Signed-off-by: Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>

--- src/lib/components/custom/hero/hero.svelte
+++ src/lib/components/custom/hero/hero.svelte
@@ -2,7 +2,7 @@
 	import Backgrounds from './backgrounds.svelte';
 	import Countdown from '$lib/components/custom/countdown/countdown.svelte';
 
-	const video = 'https://www.youtube.com/embed/EPtW0rUx8HY?si=TC1R08GFe8xWZLsk';
+	const video = 'https://www.youtube.com/embed/TdvQe6Scigc';
 	const targetDate = new Date('2025-03-11T05:00:00Z'); // WARN: Use UTC time here
 </script>
 
@@ -13,11 +13,8 @@
 	<div class="col-start-1 row-start-1 h-full w-full bg-black opacity-60"></div>
 	<div class="z-0 col-start-1 row-start-1 flex items-center justify-center text-center">
 		<div class="max-w-2xl">
-			<!--<h1 class="mb-5 animate-fadeIn text-balance text-5xl font-bold text-white sm:text-7xl">
-				BORBA NIJE GOTOVA DOK SE ZAHTEVI NE ISPUNE
-			</h1>-->
 			<h1 class="mb-5 animate-fadeIn text-balance text-5xl font-bold text-white sm:text-7xl">
-				NASTAVAK PRENOSA RTV BLOKADE JE ZA
+				BORBA NIJE GOTOVA DOK SE ZAHTEVI NE ISPUNE
 			</h1>
 			<div class="flex w-full animate-fadeIn justify-center p-2">
 				<Countdown {video} {targetDate} />
