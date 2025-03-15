0604ba405cacc56c83d9e562dbe5e5c447f4dbed
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2025-03-15 11:55:49 +0100

    fix(timer): 13h
    Signed-off-by: Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>

--- src/lib/components/custom/hero/hero.svelte
+++ src/lib/components/custom/hero/hero.svelte
@@ -3,7 +3,7 @@
 	import Countdown from '$lib/components/custom/countdown/countdown.svelte';
 
 	const video = 'https://www.youtube.com/embed/U2Dh4N_n6X8';
-	const targetDate = new Date('2025-03-15T15:00:00Z'); // WARN: Use UTC time here
+	const targetDate = new Date('2025-03-15T12:00:00Z'); // WARN: Use UTC time here
 </script>
 
 <div class="relative left-0 top-0 grid h-lvh w-full place-items-center bg-black bg-cover bg-center">
