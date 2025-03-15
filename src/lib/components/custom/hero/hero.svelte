98e9becf990cb28ed8b95b2dd27eb9d37abb972e
Matija Kljajić <64456143+matijakljajic@users.noreply.github.com>
2025-03-15 12:51:04 +0100

    fix(hero): 15. za 15

--- src/lib/components/custom/hero/hero.svelte
+++ src/lib/components/custom/hero/hero.svelte
@@ -2,8 +2,8 @@
 	import Backgrounds from './backgrounds.svelte';
 	import Countdown from '$lib/components/custom/countdown/countdown.svelte';
 
-	const video = 'https://www.youtube.com/embed/U2Dh4N_n6X8';
-	const targetDate = new Date('2025-03-15T12:00:00Z'); // WARN: Use UTC time here
+	const video = 'https://www.youtube.com/embed/_u_ZBq1AhmQ?si=VRspsD0BzqoDUCGl';
+	const targetDate = new Date('2025-03-15T12:40:00Z'); // WARN: Use UTC time here
 </script>
 
 <div class="relative left-0 top-0 grid h-lvh w-full place-items-center bg-black bg-cover bg-center">
@@ -13,7 +13,7 @@
 	<div class="col-start-1 row-start-1 h-full w-full bg-black opacity-60"></div>
 	<div class="z-0 col-start-1 row-start-1 flex items-center justify-center text-center">
 		<div class="max-w-2xl">
-			<h1 class="mb-5 animate-fadeIn text-balance text-5xl font-bold text-white sm:text-7xl">
+			<h1 class="mb-5 animate-fadeIn text-balance text-5xl font-bold text-white">
 				BORBA NIJE GOTOVA DOK SE ZAHTEVI NE ISPUNE
 			</h1>
 			<div class="flex w-full animate-fadeIn justify-center p-2">
