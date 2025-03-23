6b2cea8dbd9346c6f4b7d00df60f305c2e5ac24c
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2025-03-23 20:15:38 +0100

    fix(hero): revert home

--- src/lib/components/custom/hero/hero.svelte
+++ src/lib/components/custom/hero/hero.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import Backgrounds from './backgrounds.svelte';
-	// import Countdown from '$lib/components/custom/countdown/countdown.svelte';
+	import Countdown from '$lib/components/custom/countdown/countdown.svelte';
 
 	const video = 'https://www.youtube.com/embed/_u_ZBq1AhmQ?si=VRspsD0BzqoDUCGl';
 	const targetDate = new Date('2025-03-15T12:40:00Z'); // WARN: Use UTC time here
@@ -13,15 +13,15 @@
 	<div class="col-start-1 row-start-1 h-full w-full bg-black opacity-60"></div>
 	<div class="z-0 col-start-1 row-start-1 flex items-center justify-center text-center">
 		<div class="max-w-2xl">
-			<h1 class="mb-5 animate-fadeIn text-balance text-4xl font-bold text-white">
+			<!-- <h1 class="mb-5 animate-fadeIn text-balance text-4xl font-bold text-white">
 				PAD NADSTREŠNICE OKONČAO JE JOŠ JEDAN ŽIVOT, A ODGOVORNIH NEMA
-			</h1>
-			<!-- <h1 class="mb-5 animate-fadeIn text-balance text-5xl font-bold text-white">
+			</h1> -->
+			<h1 class="mb-5 animate-fadeIn text-balance text-5xl font-bold text-white">
 				BORBA NIJE GOTOVA DOK SE ZAHTEVI NE ISPUNE
 			</h1>
 			<div class="flex w-full animate-fadeIn justify-center p-2">
 				<Countdown {video} {targetDate} />
-			</div> -->
+			</div>
 		</div>
 	</div>
 </div>
