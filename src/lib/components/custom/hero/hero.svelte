c6a6cc520c639a9b2d6cbe0bf800c402d912eec8
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2025-04-11 22:09:52 +0200

    fix(hero): np
    Signed-off-by: Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>

--- src/lib/components/custom/hero/hero.svelte
+++ src/lib/components/custom/hero/hero.svelte
@@ -3,7 +3,7 @@
 	import Countdown from '$lib/components/custom/countdown/countdown.svelte';
 
 	const video = 'https://www.youtube.com/embed/onaBugD9sAc?si=1rBfamp6JPYxqXR_';
-	const targetDate = new Date('2025-03-27T15:00:00Z'); // WARN: Use UTC time here
+	const targetDate = new Date('2025-04-12T13:45:00Z'); // WARN: Use UTC time here
 </script>
 
 <div class="relative left-0 top-0 grid h-lvh w-full place-items-center bg-black bg-cover bg-center">
@@ -13,15 +13,12 @@
 	<div class="col-start-1 row-start-1 h-full w-full bg-black opacity-60"></div>
 	<div class="z-0 col-start-1 row-start-1 flex items-center justify-center text-center">
 		<div class="max-w-2xl">
-			<!-- <h1 class="mb-5 animate-fadeIn text-balance text-4xl font-bold text-white">
-				PAD NADSTREŠNICE OKONČAO JE JOŠ JEDAN ŽIVOT, A ODGOVORNIH NEMA
-			</h1> -->
 			<h1 class="animate-fadeIn mb-5 text-balance text-5xl font-bold text-white">
 				BORBA NIJE GOTOVA DOK SE ZAHTEVI NE ISPUNE
 			</h1>
-			<!-- <div class="flex w-full animate-fadeIn justify-center p-2">
+			<div class="flex w-full animate-fadeIn justify-center p-2">
 				<Countdown {video} {targetDate} />
-			</div> -->
+			</div>
 		</div>
 	</div>
 </div>
