73f3c800a1d69ef3fd698d17a3a6dc03751be7a8
Matija Kljajić <64456143+matijakljajic@users.noreply.github.com>
2025-04-16 22:34:14 +0200

    fix(hero): remove embed

--- src/lib/components/custom/hero/hero.svelte
+++ src/lib/components/custom/hero/hero.svelte
@@ -7,18 +7,18 @@
 </script>
 
 <div class="relative left-0 top-0 grid h-lvh w-full place-items-center bg-black bg-cover bg-center">
-	<div class="animate-fadeIn col-start-1 row-start-1 h-full w-full">
+	<div class="col-start-1 row-start-1 h-full w-full animate-fadeIn">
 		<Backgrounds />
 	</div>
 	<div class="col-start-1 row-start-1 h-full w-full bg-black opacity-60"></div>
 	<div class="z-0 col-start-1 row-start-1 flex items-center justify-center text-center">
 		<div class="max-w-2xl">
-			<h1 class="animate-fadeIn mb-5 text-balance text-5xl font-bold text-white">
+			<h1 class="mb-5 animate-fadeIn text-balance text-5xl font-bold text-white">
 				BORBA NIJE GOTOVA DOK SE ZAHTEVI NE ISPUNE
 			</h1>
-			<div class="flex w-full animate-fadeIn justify-center p-2">
+			<!--<div class="flex w-full animate-fadeIn justify-center p-2">
 				<Countdown {video} {targetDate} />
-			</div>
+			</div>-->
 		</div>
 	</div>
 </div>
