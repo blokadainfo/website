f1518611e094c3a56a91a187e22fa9230bd96d3e
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2025-10-27 10:13:34 +0100

    fix(donations): remove hitno 111

--- src/components/custom/donations/donations.svelte
+++ src/components/custom/donations/donations.svelte
@@ -8,17 +8,17 @@
   <h1 class="text-center text-3xl font-bold">Donacije za redakciju Blokada INFO</h1>
   <hr />
   <h2 class="text-center text-2xl">Novčane donacije</h2>
-  <p class="leading-tight hyphens-auto sm:text-justify">
+  <p class="hyphens-auto leading-tight sm:text-justify">
     Novčane donacije možete doneti <b>uživo na Prirodno-matematički fakultet</b> u Novom Sadu ili na
     Vaš
     <b>lokalni zbor</b>.
   </p>
-  <p class="leading-tight hyphens-auto sm:text-justify">
+  <p class="hyphens-auto leading-tight sm:text-justify">
     Takođe je moguća <b>uplata na račun</b> ako nam se <b>obratite na email</b> adresu
     <a href="mailto:donacije@blokada.info" class="text-teal-500 underline">donacije@blokada.info</a
     >.
   </p>
-  <p class="leading-tight hyphens-auto sm:text-justify">
+  <p class="hyphens-auto leading-tight sm:text-justify">
     <b>Iz inostranstva</b> možete donirati online putem portala
     <a href="https://podrzistudente.org" class="font-bold text-teal-500 underline"
       >podrzistudente.org</a
@@ -27,9 +27,6 @@
   </p>
   <hr />
   <h2 class="text-center text-2xl">Potrebna oprema za rad</h2>
-  <p class="text-center text-lg font-bold text-red-400">
-    Sve stavke sa oznakom "HITNO" su nam potrebne za prenos 31.10.2025. i 1.11.2025.
-  </p>
   <p class="text-justify leading-tight">
     U tabeli ispod se nalaze sve potrepštine našeg tima, uz priložene linkove ka stranicama gde ih
     je moguće kupiti. U slučaju da znate način da nabavite predmet za manju cenu ili da ga kupite
