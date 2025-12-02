d26d31b4f3dc0e7014ee37b37ebe85d9f5f37d49
Aleksa Siriški <31509435+aleksasiriski@users.noreply.github.com>
2025-12-02 19:17:40 +0100

    fix(donations): update list

--- src/components/custom/donations/donations.ts
+++ src/components/custom/donations/donations.ts
@@ -5,12 +5,12 @@ export const donations: {
 }[] = [
   {
     name: 'Radxa Rock 5B+ Plus (dovoljna je 4GB varijanta ali ako nema na stanju može i sa više memorije)',
-    pieces: 2,
-    links: ['https://shop.allnetchina.cn/products/rock5-model-b-1?variant=49783253827900'],
+    pieces: 1,
+    links: ['https://shop.allnetchina.cn/products/rock5-model-b-1'],
   },
   {
     name: 'SENNHEISER MD46 mikrofon',
-    pieces: 1,
+    pieces: 2,
     links: [
       'https://www.musiccentar.com/proizvod/sennheiser-md-46-mikrofon',
       'https://www.player.rs/ozvucenje/mikrofoni/mikrofoni-za-kamere/sennheiser-md-46',
@@ -18,11 +18,16 @@ export const donations: {
   },
   {
     name: 'SENNHEISER HD25 slušalice',
-    pieces: 3,
+    pieces: 5,
     links: [
       'https://www.audiopro.rs/proizvod/sennheiser-hd-25-slusalice?idPro=30251',
       'https://www.player.rs/dj/dj-slusalice/sennheiser-hd-25',
     ],
+  },
+  {
+    name: 'Coman DX428AQ6 monopod',
+    pieces: 2,
+    links: ['https://josipovic.rs/proizvod/coman-dx428aq6-monopod'],
   },
   {
     name: 'Sachtler Ace M GS 1002 Tripod stativ',
